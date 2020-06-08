0bc111052279a139cf5f356ab348cb2d183e9699
Jack Lo <jack.lo@tbcasoft.com>
2020-06-08 18:19:15 +0800

    Add adpter for recyclerView

---
+++ app/src/main/java/com/example/loadmore/DataAdapter.kt
@@ -0,0 +1,88 @@
+package com.example.loadmore
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
+import kotlinx.android.synthetic.main.progressbar.view.*
+
+class DataAdapter constructor(
+    private val studentList: List<Student>,
+    private val recyclerView: RecyclerView
+) : RecyclerView.Adapter<DataAdapter.StudentViewHolder>() {
+
+    companion object {
+        private const val TAG = "DataAdapter"
+        private const val THRESHOLD = 3
+
+        private const val VIEW_ITEM = 1
+        private const val VIEW_PROGRESS = 0
+    }
+
+    private var isLoading: Boolean = false
+    private var onLoadMoreListener: OnLoadMoreListener? = null
+
+    init {
+        if (recyclerView.layoutManager is LinearLayoutManager) {
+            val layoutManager: LinearLayoutManager =
+                recyclerView.layoutManager as LinearLayoutManager
+            val totalItemCount = layoutManager.itemCount
+            recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+
+                    val lastPosition = layoutManager.findLastVisibleItemPosition()
+                    if (isLoading && totalItemCount <= lastPosition + THRESHOLD) {
+
+                        onLoadMoreListener?.run {
+                            isLoading = true
+                            onLoadMore()
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    // Call after the loading more is finished
+    fun setLoaded() {
+        isLoading = false
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return studentList.getOrNull(position)?.run { VIEW_ITEM } ?: VIEW_PROGRESS
+    }
+
+    class StudentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvName: TextView = view.tvName
+        val tvEmailId: TextView = view.tvEmailId
+        val progressBar: ProgressBar = view.progressBar1
+
+        init {
+            view.setOnClickListener {
+                // Do nothing
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false)
+        return StudentViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = studentList.size
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        val student = studentList.getOrNull(position)
+        if (student != null) {
+            holder.tvName.text = student.name
+            holder.tvEmailId.text = student.emailId
+        } else {
+            holder.progressBar.isIndeterminate = true
+        }
+    }
+}
